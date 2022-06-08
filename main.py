fb39f46e4a77c30c4669c95dd41881075e3bfb92
harshjainsk <harshjainsk531@gmail.com>
2022-06-08 20:17:43 +0530

    cookies rejected functionality added

--- main.py
+++ main.py
@@ -16,3 +16,26 @@ driver.get("https://edalnice.cz/en/bulk-purchase/index.html#/multi_eshop/batch")
 driver.implicitly_wait(10)
 time.sleep(3)
 
+try:
+
+    country = {
+        'India': 'INDIA',
+        'French Republic': 'FR',
+        'United States': 'US',
+        'Czech Republic': 'CR',
+        'Russia': 'RU',
+    }
+
+    df = pd.read_csv('sample.csv')
+
+    """
+        Reject cookies functionality
+        """
+    clickon_cookie_rejection = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH,
+             '/html/body/footer/div[2]/div/div/div[2]/div/button[2]'))
+    )
+    clickon_cookie_rejection.click()
+    # time.sleep(0.5)
+
